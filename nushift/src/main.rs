728723070da5a0a07dc57925781146ea0cc1c145
David Pollack <david@pollack.id.au>
2020-06-07 14:11:07 +1000

    Added druid demo

--- nushift/src/main.rs
+++ nushift/src/main.rs
@@ -1,3 +1,18 @@
+use druid::widget::{Align, Label};
+use druid::{AppLauncher, WindowDesc, Widget};
+
 fn main() {
-    println!("Hello, world!");
+    let main_window = WindowDesc::new(build_root_widget)
+        .title("GUI demo");
+
+    AppLauncher::with_window(main_window)
+        .use_simple_logger()
+        .launch(())
+        .expect("Launch failed");
+}
+
+fn build_root_widget() -> impl Widget<()> {
+    let label = Label::new("Hello");
+
+    Align::centered(label)
 }
