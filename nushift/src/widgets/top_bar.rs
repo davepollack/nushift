edbd3fa3b8ba7e38151830aed3e728e121241c4e
David Pollack <david@pollack.id.au>
2020-06-14 20:32:27 +1000

    Attempt to add a unit test

--- nushift/src/widgets/top_bar.rs
+++ nushift/src/widgets/top_bar.rs
@@ -6,18 +6,47 @@ use crate::RootData;
 const TOP_BAR_HEIGHT: f64 = 30.0;
 const TOP_BAR_HORIZONTAL_PADDING: f64 = 10.0;
 
+#[cfg(test)]
+use mockall::{automock, predicate::*};
+#[cfg_attr(test, automock)]
+trait FlexTrait {
+    fn row() -> Flex<RootData>;
+}
+
+impl FlexTrait for Flex<RootData> {
+    fn row() -> Flex<RootData> { Flex::row() }
+}
+
 pub fn build_top_bar() -> impl Widget<RootData> {
+    build_top_bar_internal::<Flex<RootData>>()
+}
+
+fn build_top_bar_internal<FlexI: FlexTrait>() -> impl Widget<RootData> {
 
     let tab_title = Label::new(LocalizedString::new("new-tab"))
         .with_text_color(Color::grey8(0x00));
 
-    let tab_bar = Flex::row();
+    let tab_bar = FlexI::row();
 
-    Flex::row()
+    FlexI::row()
         .cross_axis_alignment(CrossAxisAlignment::Center)
         .with_flex_child(tab_title, 2.0)
         .with_flex_child(tab_bar, 3.0)
         .fix_height(TOP_BAR_HEIGHT)
         .padding((TOP_BAR_HORIZONTAL_PADDING, 0.))
         .background(Color::rgb8(0x82, 0xe0, 0xe0))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn build_top_bar_creates_two_flex_rows() {
+        let ctx = MockFlexTrait::row_context();
+        ctx.expect()
+            .times(2)
+            .returning(|| Flex::row());
+        build_top_bar_internal::<MockFlexTrait>();
+    }
 }
