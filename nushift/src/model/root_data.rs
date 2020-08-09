afdf71eeb2588e431bf0ab40fd0bbbf3831ce3d5
David Pollack <david@pollack.id.au>
2020-08-09 15:41:53 +1000

    Added tests for close_tab

--- nushift/src/model/root_data.rs
+++ nushift/src/model/root_data.rs
@@ -55,16 +55,16 @@ impl RootData {
 
         match (&id_to_remove, &self.currently_selected_tab_id, index_to_remove) {
             // First tab was closed, is currently selected, and there are no tabs left
-            (Some(id), Some(currently_selected_tab_id), Some(0)) if id.id_eq(currently_selected_tab_id) && self.tabs.is_empty() => {
+            (Some(id_to_remove), Some(currently_selected_tab_id), Some(0)) if id_to_remove.id_eq(currently_selected_tab_id) && self.tabs.is_empty() => {
                 self.currently_selected_tab_id = None;
             }
             // First tab was closed, is currently selected, and there are still some tabs left
-            (Some(id), Some(currently_selected_tab_id), Some(0)) if id.id_eq(currently_selected_tab_id) => {
+            (Some(id_to_remove), Some(currently_selected_tab_id), Some(0)) if id_to_remove.id_eq(currently_selected_tab_id) => {
                 let first_tab_id = Arc::clone(&self.tabs[0].id);
                 self.currently_selected_tab_id = Some(first_tab_id);
             },
             // Other tab was closed, is currently selected
-            (Some(id), Some(currently_selected_tab_id), Some(index)) if id.id_eq(currently_selected_tab_id) => {
+            (Some(id_to_remove), Some(currently_selected_tab_id), Some(index)) if id_to_remove.id_eq(currently_selected_tab_id) => {
                 let previous_tab_id = Arc::clone(&self.tabs[index - 1].id);
                 self.currently_selected_tab_id = Some(previous_tab_id);
             },
@@ -82,6 +82,7 @@ impl RootData {
 pub mod tests {
     use super::*;
     use druid::im::vector;
+    use nushift_core::ReusableIdPool;
 
     pub fn mock() -> RootData {
         let hypervisor = Arc::new(Mutex::new(Hypervisor::new()));
@@ -118,5 +119,72 @@ pub mod tests {
         assert!(root_data.currently_selected_tab_id.as_ref().unwrap().id_eq(&tab1));
     }
 
-    // TODO add tests for close_tab
+    #[test]
+    fn close_tab_should_remove_from_tabs_vector() {
+        let mut root_data = mock();
+        let tab1 = root_data.add_new_tab();
+
+        root_data.close_tab(&tab1);
+
+        assert!(root_data.tabs.is_empty());
+    }
+
+    #[test]
+    fn close_tab_should_set_currently_selected_to_none_if_no_tabs_left() {
+        let mut root_data = mock();
+        let tab1 = root_data.add_new_tab();
+
+        root_data.close_tab(&tab1);
+
+        assert!(root_data.currently_selected_tab_id.is_none());
+    }
+
+    #[test]
+    fn close_tab_should_set_currently_selected_to_next_tab_if_first_tab_was_closed() {
+        let mut root_data = mock();
+        let tab1 = root_data.add_new_tab();
+        let tab2 = root_data.add_new_tab();
+        root_data.select_tab(&tab1);
+
+        root_data.close_tab(&tab1);
+
+        assert!(root_data.currently_selected_tab_id.as_ref().unwrap().id_eq(&tab2));
+    }
+
+    #[test]
+    fn close_tab_should_set_currently_selected_to_previous_tab_if_tab_other_than_first_was_closed() {
+        let mut root_data = mock();
+        let tab1 = root_data.add_new_tab();
+        let tab2 = root_data.add_new_tab();
+
+        root_data.close_tab(&tab2);
+
+        assert!(root_data.currently_selected_tab_id.as_ref().unwrap().id_eq(&tab1));
+    }
+
+    #[test]
+    fn close_tab_should_not_set_currently_selected_if_not_currently_selected_tab_was_closed() {
+        let mut root_data = mock();
+        let tab1 = root_data.add_new_tab();
+        let tab2 = root_data.add_new_tab();
+
+        root_data.close_tab(&tab1);
+
+        assert!(root_data.currently_selected_tab_id.as_ref().unwrap().id_eq(&tab2));
+    }
+
+    #[test]
+    fn close_tab_should_do_nothing_if_other_id_is_passed_in() {
+        let mut root_data = mock();
+        let _tab1 = root_data.add_new_tab();
+        let tab2 = root_data.add_new_tab();
+        let reusable_id_pool = Arc::new(Mutex::new(ReusableIdPool::new()));
+        let other_id = ReusableIdPool::allocate(&reusable_id_pool);
+
+        root_data.close_tab(&other_id);
+
+        assert_eq!(2, root_data.tabs.len());
+        assert!(root_data.currently_selected_tab_id.is_some());
+        assert!(root_data.currently_selected_tab_id.as_ref().unwrap().id_eq(&tab2));
+    }
 }
