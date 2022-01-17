44154c930660773ca3d4b33675656cffa6ca797a
David Pollack <david@pollack.id.au>
2022-01-17 22:10:05 +1100

    Added stack

--- nushift-core/src/riscv_machine_wrapper.rs
+++ nushift-core/src/riscv_machine_wrapper.rs
@@ -1,4 +1,5 @@
-use riscy_emulator::{memory::Memory, machine::RiscvMachine};
+use riscy_emulator::{memory::{Memory, Region}, machine::RiscvMachine};
+use riscy_isa::Register;
 
 use crate::nushift_subsystem::NushiftSubsystem;
 
@@ -6,8 +7,22 @@ pub struct RiscvMachineWrapper;
 
 impl RiscvMachineWrapper {
     pub fn new() -> RiscvMachine<NushiftSubsystem> {
-        let memory = Memory::new();
+        let mut memory = Memory::new();
+
+        // The stack. 256 KiB.
+        //
+        // Should the location and size be determined by app metadata? Should
+        // the location be randomised?
+        const STACK_BASE: u64 = 0x80000000;
+        const STACK_SIZE: u64 = 0x40000;
+        let stack = Region::readwrite_memory(STACK_BASE, STACK_SIZE);
+
+        memory.add_region(stack);
+
         let entry = 0u64; // TODO: Make real.
-        RiscvMachine::new(memory, entry)
+        let mut machine = RiscvMachine::new(memory, entry);
+        machine.state_mut().registers.set(Register::StackPointer, STACK_BASE + STACK_SIZE);
+
+        machine
     }
 }
