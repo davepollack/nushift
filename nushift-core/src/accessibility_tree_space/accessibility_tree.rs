e250629f04359f29ce686e2311536d58e76cf0cf
David Pollack <david@pollack.id.au>
2023-08-15 16:40:20 +1000

    Specified `aabb` as virtual points

--- nushift-core/src/accessibility_tree_space/accessibility_tree.rs
+++ nushift-core/src/accessibility_tree_space/accessibility_tree.rs
@@ -1,19 +1,26 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
 pub struct AccessibilityTree {
     surfaces: Vec<Surface>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
 struct Surface {
     display_list: Vec<DisplayItem>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
 enum DisplayItem {
     Text {
-        aabb: (Vec<f64>, Vec<f64>),
+        aabb: (Vec<VirtualPoint>, Vec<VirtualPoint>),
         text: String,
     },
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(transparent)]
+struct VirtualPoint(f64);
