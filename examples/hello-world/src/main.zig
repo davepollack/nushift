b6134daf07725cfba17b535e3ab9f3d47d229315
David Pollack <david@pollack.id.au>
2023-09-28 13:00:46 +1000

    Return errors from calls to write_str_to_input_cap

--- examples/hello-world/src/main.zig
+++ examples/hello-world/src/main.zig
@@ -8,18 +8,23 @@ const TITLE_INPUT_ACQUIRE_ADDRESS: usize = 0x90000000;
 const A11Y_INPUT_ACQUIRE_ADDRESS: usize = 0x90001000;
 
 pub fn main() usize {
-    return main_impl() catch |err| OsNushift.errorCodeFromSyscallError(err);
+    return main_impl() catch |err| switch (err) {
+        // When https://github.com/ziglang/zig/issues/2473 is complete, we can
+        // do that instead of inline else.
+        inline else => |any_err| if (std.meta.fieldIndex(OsNushift.SyscallError, @errorName(any_err))) |_| blk: {
+            break :blk OsNushift.errorCodeFromSyscallError(@field(OsNushift.SyscallError, @errorName(any_err)));
+        } else 1,
+    };
 }
 
-fn main_impl() OsNushift.SyscallError!usize {
+fn main_impl() (std.io.FixedBufferStream([]u8).WriteError || OsNushift.SyscallError)!usize {
     const title_cap_id = try OsNushift.syscall(.title_new, .{});
     defer _ = OsNushift.syscall_ignore_errors(.title_destroy, .{ .title_cap_id = title_cap_id });
 
     const title_input_shm_cap_id = try OsNushift.syscall(.shm_new_and_acquire, .{ .shm_type = OsNushift.ShmType.four_kib, .length = 1, .address = TITLE_INPUT_ACQUIRE_ADDRESS });
     defer _ = OsNushift.syscall_ignore_errors(.shm_release_and_destroy, .{ .shm_cap_id = title_input_shm_cap_id });
 
-    // TODO: Return an `error.`
-    write_str_to_input_cap(@as([*]u8, @ptrFromInt(TITLE_INPUT_ACQUIRE_ADDRESS))[0..4096], title) catch return 1;
+    try write_str_to_input_cap(@as([*]u8, @ptrFromInt(TITLE_INPUT_ACQUIRE_ADDRESS))[0..4096], title);
 
     const title_task_id = try OsNushift.syscall(.title_publish, .{ .title_cap_id = title_cap_id, .input_shm_cap_id = title_input_shm_cap_id });
     _ = title_task_id;
@@ -34,8 +39,7 @@ fn main_impl() OsNushift.SyscallError!usize {
     const a11y_input_shm_cap_id = try OsNushift.syscall(.shm_new_and_acquire, .{ .shm_type = OsNushift.ShmType.four_kib, .length = 10, .address = A11Y_INPUT_ACQUIRE_ADDRESS });
     defer _ = OsNushift.syscall_ignore_errors(.shm_release_and_destroy, .{ .shm_cap_id = a11y_input_shm_cap_id });
 
-    // TODO: Return an `error.`
-    write_str_to_input_cap(@as([*]u8, @ptrFromInt(A11Y_INPUT_ACQUIRE_ADDRESS))[0..40960], ron) catch return 1;
+    try write_str_to_input_cap(@as([*]u8, @ptrFromInt(A11Y_INPUT_ACQUIRE_ADDRESS))[0..40960], ron);
 
     const a11y_task_id = try OsNushift.syscall(.accessibility_tree_publish, .{ .accessibility_tree_cap_id = a11y_tree_cap_id, .input_shm_cap_id = a11y_input_shm_cap_id });
     _ = a11y_task_id;
