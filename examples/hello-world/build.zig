8086a44978fc0bb8f82d4dbdd4bf7c6555d9a1df
David Pollack <david@pollack.id.au>
2023-10-26 14:11:03 +1100

    Changed std.build.Builder to std.Build

--- examples/hello-world/build.zig
+++ examples/hello-world/build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const BuildNushift = @import("./build_nushift.zig");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     BuildNushift.build(b, "hello-world", "src/main.zig", true, null);
 }
