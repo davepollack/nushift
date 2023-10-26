abf973d4d380360124a51ab8d113c0934264a9d2
David Pollack <david@pollack.id.au>
2023-10-26 17:34:27 +1100

    Broke main out into functions/structs
    And for some reason this decreased the binary size, probably due to not
    having so many defers in one block. When I initially added the code, the
    binary size did blow up more than I was expecting.

--- examples/hello-world/src/main.zig
+++ examples/hello-world/src/main.zig
@@ -21,50 +21,113 @@ pub fn main() usize {
 }
 
 fn main_impl() (FBSWriteError || OsNushift.SyscallError)!usize {
-    const title_cap_id = try OsNushift.syscall(.title_new, .{});
-    defer _ = OsNushift.syscall_ignore_errors(.title_destroy, .{ .title_cap_id = title_cap_id });
+    const tasks = blk: {
+        const title_task = try TitleTask.init();
+        errdefer title_task.deinit();
 
-    const title_input_shm_cap_id = try OsNushift.syscall(.shm_new_and_acquire, .{ .shm_type = OsNushift.ShmType.four_kib, .length = 1, .address = TITLE_INPUT_ACQUIRE_ADDRESS });
-    defer _ = OsNushift.syscall_ignore_errors(.shm_release_and_destroy, .{ .shm_cap_id = title_input_shm_cap_id });
+        const a11y_tree_task = try AccessibilityTreeTask.init();
+        errdefer a11y_tree_task.deinit();
 
-    try write_str_to_input_cap(@as([*]u8, @ptrFromInt(TITLE_INPUT_ACQUIRE_ADDRESS))[0..4096], title);
+        break :blk .{ title_task, a11y_tree_task };
+    };
 
-    const title_output_shm_cap_id = try OsNushift.syscall(.shm_new, .{ .shm_type = OsNushift.ShmType.four_kib, .length = 1 });
-    defer _ = OsNushift.syscall_ignore_errors(.shm_destroy, .{ .shm_cap_id = title_output_shm_cap_id });
+    const title_task_id = try tasks[0].title_publish();
+    const a11y_task_id = try tasks[1].accessibility_tree_publish();
 
-    const title_task_id = try OsNushift.syscall(.title_publish, .{ .title_cap_id = title_cap_id, .input_shm_cap_id = title_input_shm_cap_id, .output_shm_cap_id = title_output_shm_cap_id });
+    // If an error occurs between publishing and the end of
+    // block_on_deferred_tasks, you can't deinit the tasks because the resources
+    // are in-flight. And we don't. But that does mean the task resources will
+    // leak if that error occurs.
 
-    // TODO: Title cap, input cap and output cap should be destroyed after
-    // deferred task is finished. The current defer statements mean this is
-    // indeed happening, just not immediately after the deferred task is
-    // finished.
+    try block_on_deferred_tasks(&.{ title_task_id, a11y_task_id });
 
-    const a11y_tree_cap_id = try OsNushift.syscall(.accessibility_tree_new, .{});
-    defer _ = OsNushift.syscall_ignore_errors(.accessibility_tree_destroy, .{ .accessibility_tree_cap_id = a11y_tree_cap_id });
+    tasks[1].deinit();
+    tasks[0].deinit();
 
-    const a11y_input_shm_cap_id = try OsNushift.syscall(.shm_new_and_acquire, .{ .shm_type = OsNushift.ShmType.four_kib, .length = 10, .address = A11Y_INPUT_ACQUIRE_ADDRESS });
-    defer _ = OsNushift.syscall_ignore_errors(.shm_release_and_destroy, .{ .shm_cap_id = a11y_input_shm_cap_id });
+    return 1000;
+}
 
-    try write_str_to_input_cap(@as([*]u8, @ptrFromInt(A11Y_INPUT_ACQUIRE_ADDRESS))[0..40960], ron);
+const TitleTask = struct {
+    title_cap_id: usize,
+    title_input_shm_cap_id: usize,
+    title_output_shm_cap_id: usize,
 
-    const a11y_output_shm_cap_id = try OsNushift.syscall(.shm_new, .{ .shm_type = OsNushift.ShmType.four_kib, .length = 1 });
-    defer _ = OsNushift.syscall_ignore_errors(.shm_destroy, .{ .shm_cap_id = a11y_output_shm_cap_id });
+    const Self = @This();
 
-    const a11y_task_id = try OsNushift.syscall(.accessibility_tree_publish, .{ .accessibility_tree_cap_id = a11y_tree_cap_id, .input_shm_cap_id = a11y_input_shm_cap_id, .output_shm_cap_id = a11y_output_shm_cap_id });
+    fn init() (FBSWriteError || OsNushift.SyscallError)!Self {
+        const title_cap_id = try OsNushift.syscall(.title_new, .{});
+        errdefer _ = OsNushift.syscall_ignore_errors(.title_destroy, .{ .title_cap_id = title_cap_id });
 
+        const title_input_shm_cap_id = try OsNushift.syscall(.shm_new_and_acquire, .{ .shm_type = OsNushift.ShmType.four_kib, .length = 1, .address = TITLE_INPUT_ACQUIRE_ADDRESS });
+        errdefer _ = OsNushift.syscall_ignore_errors(.shm_release_and_destroy, .{ .shm_cap_id = title_input_shm_cap_id });
+
+        try write_str_to_input_cap(@as([*]u8, @ptrFromInt(TITLE_INPUT_ACQUIRE_ADDRESS))[0..4096], title);
+
+        const title_output_shm_cap_id = try OsNushift.syscall(.shm_new, .{ .shm_type = OsNushift.ShmType.four_kib, .length = 1 });
+        errdefer _ = OsNushift.syscall_ignore_errors(.shm_destroy, .{ .shm_cap_id = title_output_shm_cap_id });
+
+        return Self{
+            .title_cap_id = title_cap_id,
+            .title_input_shm_cap_id = title_input_shm_cap_id,
+            .title_output_shm_cap_id = title_output_shm_cap_id,
+        };
+    }
+
+    fn deinit(self: Self) void {
+        _ = OsNushift.syscall_ignore_errors(.shm_destroy, .{ .shm_cap_id = self.title_output_shm_cap_id });
+        _ = OsNushift.syscall_ignore_errors(.shm_release_and_destroy, .{ .shm_cap_id = self.title_input_shm_cap_id });
+        _ = OsNushift.syscall_ignore_errors(.title_destroy, .{ .title_cap_id = self.title_cap_id });
+    }
+
+    fn title_publish(self: *const Self) OsNushift.SyscallError!usize {
+        return OsNushift.syscall(.title_publish, .{ .title_cap_id = self.title_cap_id, .input_shm_cap_id = self.title_input_shm_cap_id, .output_shm_cap_id = self.title_output_shm_cap_id });
+    }
+};
+
+const AccessibilityTreeTask = struct {
+    a11y_tree_cap_id: usize,
+    a11y_input_shm_cap_id: usize,
+    a11y_output_shm_cap_id: usize,
+
+    const Self = @This();
+
+    fn init() (FBSWriteError || OsNushift.SyscallError)!Self {
+        const a11y_tree_cap_id = try OsNushift.syscall(.accessibility_tree_new, .{});
+        errdefer _ = OsNushift.syscall_ignore_errors(.accessibility_tree_destroy, .{ .accessibility_tree_cap_id = a11y_tree_cap_id });
+
+        const a11y_input_shm_cap_id = try OsNushift.syscall(.shm_new_and_acquire, .{ .shm_type = OsNushift.ShmType.four_kib, .length = 10, .address = A11Y_INPUT_ACQUIRE_ADDRESS });
+        errdefer _ = OsNushift.syscall_ignore_errors(.shm_release_and_destroy, .{ .shm_cap_id = a11y_input_shm_cap_id });
+
+        try write_str_to_input_cap(@as([*]u8, @ptrFromInt(A11Y_INPUT_ACQUIRE_ADDRESS))[0..40960], ron);
+
+        const a11y_output_shm_cap_id = try OsNushift.syscall(.shm_new, .{ .shm_type = OsNushift.ShmType.four_kib, .length = 1 });
+        errdefer _ = OsNushift.syscall_ignore_errors(.shm_destroy, .{ .shm_cap_id = a11y_output_shm_cap_id });
+
+        return Self{
+            .a11y_tree_cap_id = a11y_tree_cap_id,
+            .a11y_input_shm_cap_id = a11y_input_shm_cap_id,
+            .a11y_output_shm_cap_id = a11y_output_shm_cap_id,
+        };
+    }
+
+    fn deinit(self: Self) void {
+        _ = OsNushift.syscall_ignore_errors(.shm_destroy, .{ .shm_cap_id = self.a11y_output_shm_cap_id });
+        _ = OsNushift.syscall_ignore_errors(.shm_release_and_destroy, .{ .shm_cap_id = self.a11y_input_shm_cap_id });
+        _ = OsNushift.syscall_ignore_errors(.accessibility_tree_destroy, .{ .accessibility_tree_cap_id = self.a11y_tree_cap_id });
+    }
+
+    fn accessibility_tree_publish(self: *const Self) OsNushift.SyscallError!usize {
+        return OsNushift.syscall(.accessibility_tree_publish, .{ .accessibility_tree_cap_id = self.a11y_tree_cap_id, .input_shm_cap_id = self.a11y_input_shm_cap_id, .output_shm_cap_id = self.a11y_output_shm_cap_id });
+    }
+};
+
+fn block_on_deferred_tasks(task_ids: []const u64) (FBSWriteError || OsNushift.SyscallError)!void {
     const block_on_deferred_tasks_input_cap_id = try OsNushift.syscall(.shm_new_and_acquire, .{ .shm_type = OsNushift.ShmType.four_kib, .length = 1, .address = BODT_INPUT_ACQUIRE_ADDRESS });
     defer _ = OsNushift.syscall_ignore_errors(.shm_release_and_destroy, .{ .shm_cap_id = block_on_deferred_tasks_input_cap_id });
 
-    try write_task_ids_to_input_cap(@as([*]u8, @ptrFromInt(BODT_INPUT_ACQUIRE_ADDRESS))[0..4096], &.{ title_task_id, a11y_task_id });
+    try write_task_ids_to_input_cap(@as([*]u8, @ptrFromInt(BODT_INPUT_ACQUIRE_ADDRESS))[0..4096], task_ids);
 
     _ = try OsNushift.syscall(.block_on_deferred_tasks, .{ .input_shm_cap_id = block_on_deferred_tasks_input_cap_id });
-
-    // TODO: Accessibility cap, input cap and output cap should be destroyed
-    // after deferred task is finished. The current defer statements mean this
-    // is indeed happening, just not immediately after the deferred task is
-    // finished.
-
-    return a11y_tree_cap_id + 1000;
 }
 
 fn write_str_to_input_cap(input_cap_buffer: []u8, str: []const u8) FBSWriteError!void {
