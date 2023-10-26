6f74e7207edfe3aa2ef97cb945b673be737eeabe
David Pollack <david@pollack.id.au>
2023-10-26 16:01:44 +1100

    Added qoi dependency and QOI image

--- examples/hello-world/build.zig
+++ examples/hello-world/build.zig
@@ -1,6 +1,12 @@
 const std = @import("std");
 const BuildNushift = @import("./build_nushift.zig");
 
+const qoi_path = @import("root").dependencies.build_root.qoi;
+
 pub fn build(b: *std.Build) void {
-    BuildNushift.build(b, "hello-world", "src/main.zig", true, null);
+    BuildNushift.build(b, "hello-world", "src/main.zig", true, addDependencies);
+}
+
+fn addDependencies(b: *std.Build, exe: *std.Build.Step.Compile) void {
+    exe.addAnonymousModule("qoi", .{ .source_file = .{ .path = b.pathJoin(&.{ qoi_path, "src/qoi.zig" }) } });
 }
