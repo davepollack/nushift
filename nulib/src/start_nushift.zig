9c1bc1726339282003f5314714be1482cf1227a7
David Pollack <david@pollack.id.au>
2023-11-09 18:30:42 +1100

    Changed start_nushift.zig to compile with Zig 0.11.0 and later

--- nulib/src/start_nushift.zig
+++ nulib/src/start_nushift.zig
@@ -2,26 +2,38 @@ const std = @import("std");
 const os_nushift = @import("os_nushift");
 const main = @import("main");
 
+// The stack. 256 KiB, but you can change it if you want.
+const STACK_END: usize = 0x80000000;
+const STACK_NUMBER_OF_4_KIB_PAGES: usize = 64;
+
 export fn _start() callconv(.Naked) noreturn {
-    const shm_cap_id = initStack();
-    const exit_reason = main.main();
-    deinitStack(shm_cap_id);
-    _ = os_nushift.syscallIgnoreErrors(.exit, .{ .exit_reason = exit_reason });
-    unreachable;
-}
+    // Since Zig 0.11.0, this has to be inline assembly code rather than Zig
+    // code, due to restrictions placed on naked functions.
 
-fn initStack() usize {
-    // The stack. 256 KiB, but you can change it if you want.
-    const STACK_END: usize = 0x80000000;
-    const STACK_NUMBER_OF_4_KIB_PAGES: usize = 64;
+    var a0_output: usize = undefined;
+    var t0_output: usize = undefined;
 
-    const shm_cap_id = os_nushift.syscall(.shm_new_and_acquire, .{ .shm_type = os_nushift.ShmType.four_kib, .length = STACK_NUMBER_OF_4_KIB_PAGES, .address = STACK_END }) catch {
-        // Hardcode the .exit_reason rather than using the error from
-        // .shm_new_and_acquire. Because if we do the latter, the stack is used
-        // before we initialise the stack :( including in the happy path.
-        _ = os_nushift.syscallIgnoreErrors(.exit, .{ .exit_reason = @intFromEnum(os_nushift.SyscallErrorEnum.internal_error) });
-        unreachable;
-    };
+    // Init stack
+    asm volatile ("ecall"
+        : [ret_a0] "={a0}" (a0_output),
+          [ret_t0] "={t0}" (t0_output),
+        : [syscall_number] "{a0}" (os_nushift.Syscall.shm_new_and_acquire),
+          [shm_type] "{a1}" (os_nushift.ShmType.four_kib),
+          [length] "{a2}" (STACK_NUMBER_OF_4_KIB_PAGES),
+          [address] "{a3}" (STACK_END),
+        : "memory", "t0", "a0", "a1", "a2", "a3"
+    );
+
+    // If error initing stack, exit
+    if (a0_output == std.math.maxInt(usize)) {
+        asm volatile ("ecall"
+            :
+            : [syscall_number] "{a0}" (os_nushift.Syscall.exit),
+              [exit_reason] "{a1}" (t0_output),
+            : "memory", "t0", "a0", "a1"
+        );
+        // Putting `unreachable` here is not allowed since Zig 0.11.0, even though I would like to
+    }
 
     // Set SP to base
     asm volatile (""
@@ -30,9 +42,26 @@ fn initStack() usize {
         : "sp"
     );
 
-    return shm_cap_id;
-}
+    // Call main
+    const exit_reason = asm volatile ("call %[main]@plt"
+        : [exit_reason] "={a0}" (-> usize),
+        : [main] "X" (&main.main),
+        : "memory", "ra", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7"
+    );
 
-fn deinitStack(shm_cap_id: usize) void {
-    _ = os_nushift.syscallIgnoreErrors(.shm_release_and_destroy, .{ .shm_cap_id = shm_cap_id });
+    // Deinit stack, ignoring errors
+    asm volatile ("ecall"
+        :
+        : [syscall_number] "{a0}" (os_nushift.Syscall.shm_release_and_destroy),
+          [shm_cap_id] "{a1}" (a0_output),
+        : "memory", "t0", "a0", "a1"
+    );
+
+    // Exit
+    asm volatile ("ecall"
+        :
+        : [syscall_number] "{a0}" (os_nushift.Syscall.exit),
+          [exit_reason] "{a1}" (exit_reason),
+        : "memory", "t0", "a0", "a1"
+    );
 }
