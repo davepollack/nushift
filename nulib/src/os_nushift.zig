e6e81d285555fe114b5079abe41c85756e3eb0eb
David Pollack <david@pollack.id.au>
2023-12-05 17:05:35 +1100

    Added license headers and license to nulib and examples

--- nulib/src/os_nushift.zig
+++ nulib/src/os_nushift.zig
@@ -1,3 +1,9 @@
+// Copyright 2023 The Nushift Authors.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+
 const std = @import("std");
 
 pub const Syscall = enum(usize) {
