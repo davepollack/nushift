2f4d9a11fafee7b465e5e334982e1e3706d7cae2
David Pollack <david@pollack.id.au>
2023-12-14 16:42:58 +1100

    Added crate root doc

--- reusable-id-pool/src/lib.rs
+++ reusable-id-pool/src/lib.rs
@@ -6,6 +6,18 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+//! A pool for RAII IDs.
+//!
+//! ## Example
+//!
+//! ```rust
+//! use reusable_id_pool::ReusableIdPool;
+//!
+//! let reusable_id_pool = ReusableIdPool::new();
+//! let id = reusable_id_pool.allocate();
+//! // Do something with the `id`, like move it into a struct. It will be returned to the pool when it is dropped.
+//! ```
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use core::fmt::{self, Display, Debug};
