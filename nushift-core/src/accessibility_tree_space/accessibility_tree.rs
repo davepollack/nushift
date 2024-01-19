69f8a6bad231f572a4441a4dd27458576db67864
David Pollack <david@pollack.id.au>
2024-01-19 15:28:22 +1100

    Added more derives to AccessibilityTree for possible future usage in Druid

--- nushift-core/src/accessibility_tree_space/accessibility_tree.rs
+++ nushift-core/src/accessibility_tree_space/accessibility_tree.rs
@@ -3,19 +3,19 @@
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct AccessibilityTree {
     surfaces: Vec<Surface>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
 #[serde(deny_unknown_fields)]
 struct Surface {
     display_list: Vec<DisplayItem>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
 #[serde(deny_unknown_fields)]
 enum DisplayItem {
     Text {
@@ -24,6 +24,6 @@ enum DisplayItem {
     },
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
 #[serde(transparent)]
 struct VirtualPoint(f64);
