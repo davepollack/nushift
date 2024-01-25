8075217c7473430181bff6417063b526f3c7e123
David Pollack <david@pollack.id.au>
2024-01-25 18:05:14 +1100

    Added AccessibilityTree.zig

---
+++ examples/hello-world/src/AccessibilityTree.zig
@@ -0,0 +1,66 @@
+// Copyright 2023 The Nushift Authors.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const AccessibilityTree = @This();
+
+allocator: Allocator,
+surfaces: std.ArrayList(Surface),
+
+const Surface = struct {
+    const Self = @This();
+
+    display_list: std.ArrayList(DisplayItem),
+
+    const DisplayItem = union(DisplayItems) {
+        text: struct { aabb: struct { std.ArrayList(VirtualPoint), std.ArrayList(VirtualPoint) }, text: std.ArrayList(u8) },
+    };
+
+    const DisplayItems = enum {
+        text,
+    };
+
+    const VirtualPoint = f64;
+};
+
+pub fn initOneItem(allocator: Allocator) Allocator.Error!AccessibilityTree {
+    const display_item = Surface.DisplayItem{
+        .text = .{
+            .aabb = .{ std.ArrayList(Surface.VirtualPoint).init(allocator), std.ArrayList(Surface.VirtualPoint).init(allocator) },
+            .text = std.ArrayList(u8).init(allocator),
+        },
+    };
+
+    const surface = Surface{
+        .display_list = std.ArrayList(Surface.DisplayItem).init(allocator),
+    };
+    try surface.display_list.append(display_item);
+
+    const a11y_tree = AccessibilityTree{
+        .surfaces = std.ArrayList(Surface).init(allocator),
+    };
+    try a11y_tree.surfaces.append(surface);
+
+    return a11y_tree;
+}
+
+pub fn deinit(self: *AccessibilityTree) void {
+    for (self.surfaces) |surface| {
+        for (surface.display_list) |display_item| {
+            switch (display_item) {
+                .text => |text_item| {
+                    text_item.aabb[0].deinit();
+                    text_item.aabb[1].deinit();
+                    text_item.text.deinit();
+                },
+            }
+        }
+        surface.display_list.deinit();
+    }
+    self.surfaces.deinit();
+}
