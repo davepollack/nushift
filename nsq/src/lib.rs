5f43e70e0a581c10b6befa57d3748f9273ff3edd
David Pollack <david@pollack.id.au>
2024-05-24 17:06:02 +1000

    Added NsqClient entry point

--- nsq/src/lib.rs
+++ nsq/src/lib.rs
@@ -5,7 +5,7 @@ use std::io;
 use std::net::UdpSocket;
 use std::sync::Arc;
 
-use quinn::{AsyncUdpSocket, Endpoint, EndpointConfig, Runtime, ServerConfig};
+use quinn::{AsyncUdpSocket, ClientConfig, Endpoint, EndpointConfig, Runtime, ServerConfig};
 
 use crate::quinn_noise::config::{NoiseConfig, NoiseHandshakeTokenKey, NoiseHmacKey};
 use crate::quinn_noise::NSQ_QUIC_VERSION;
@@ -98,6 +98,83 @@ impl NsqServer {
             socket,
             runtime,
         ).map(Self)
+    }
+
+    pub fn endpoint(self) -> Endpoint {
+        self.0
+    }
+
+    pub fn endpoint_as_ref(&self) -> &Endpoint {
+        &self.0
+    }
+}
+
+pub struct NsqClientConfig(ClientConfig);
+
+impl NsqClientConfig {
+    pub fn new<LS>(local_static_secret: LS) -> Self
+    where
+        LS: AsRef<[u8]> + Send + Sync + 'static,
+    {
+        let mut client_config = ClientConfig::new(Arc::new(NoiseConfig::new(local_static_secret)));
+        client_config.version(NSQ_QUIC_VERSION);
+        Self(client_config)
+    }
+
+    pub fn client_config(self) -> ClientConfig {
+        self.0
+    }
+
+    pub fn client_config_as_ref(&self) -> &ClientConfig {
+        &self.0
+    }
+}
+
+pub struct NsqClient(Endpoint);
+
+impl NsqClient {
+    pub fn new_v4<LS>(local_static_secret: LS, runtime: Arc<dyn Runtime>) -> io::Result<Self>
+    where
+        LS: AsRef<[u8]> + Send + Sync + 'static,
+    {
+        let socket = UdpSocket::bind("0.0.0.0:0")?;
+        Self::new_with_socket(local_static_secret, socket, runtime)
+    }
+
+    pub fn new_v6<LS>(local_static_secret: LS, runtime: Arc<dyn Runtime>) -> io::Result<Self>
+    where
+        LS: AsRef<[u8]> + Send + Sync + 'static,
+    {
+        let socket = UdpSocket::bind("[::]:0")?;
+        Self::new_with_socket(local_static_secret, socket, runtime)
+    }
+
+    pub fn new_with_socket<LS>(local_static_secret: LS, socket: UdpSocket, runtime: Arc<dyn Runtime>) -> io::Result<Self>
+    where
+        LS: AsRef<[u8]> + Send + Sync + 'static,
+    {
+        let mut endpoint = Endpoint::new(
+            NsqEndpointConfig::new().endpoint_config(),
+            None,
+            socket,
+            runtime,
+        )?;
+        endpoint.set_default_client_config(NsqClientConfig::new(local_static_secret).client_config());
+        Ok(Self(endpoint))
+    }
+
+    pub fn new_with_abstract_socket<LS>(local_static_secret: LS, socket: Arc<dyn AsyncUdpSocket>, runtime: Arc<dyn Runtime>) -> io::Result<Self>
+    where
+        LS: AsRef<[u8]> + Send + Sync + 'static,
+    {
+        let mut endpoint = Endpoint::new_with_abstract_socket(
+            NsqEndpointConfig::new().endpoint_config(),
+            None,
+            socket,
+            runtime,
+        )?;
+        endpoint.set_default_client_config(NsqClientConfig::new(local_static_secret).client_config());
+        Ok(Self(endpoint))
     }
 
     pub fn endpoint(self) -> Endpoint {
