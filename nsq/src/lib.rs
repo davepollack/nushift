24f7e1f28055aaf10016da95b9e3cbc812b04ae3
David Pollack <david@pollack.id.au>
2024-05-24 15:24:32 +1000

    Moved some root items to config.rs, fixed clippy issue

--- nsq/src/lib.rs
+++ nsq/src/lib.rs
@@ -6,8 +6,6 @@ use std::net::UdpSocket;
 use std::sync::Arc;
 
 use quinn::{AsyncUdpSocket, Endpoint, EndpointConfig, Runtime, ServerConfig};
-use snow::resolvers::{BoxedCryptoResolver, DefaultResolver, FallbackResolver};
-use snow_x448_resolver::SnowX448Resolver;
 
 use crate::quinn_noise::config::{NoiseConfig, NoiseHandshakeTokenKey, NoiseHmacKey};
 use crate::quinn_noise::NSQ_QUIC_VERSION;
@@ -87,7 +85,7 @@ impl NsqServer {
             Some(NsqServerConfig::new(local_static_secret).server_config()),
             socket,
             runtime,
-        ).map(|endpoint| Self(endpoint))
+        ).map(Self)
     }
 
     pub fn new_with_abstract_socket<LS>(local_static_secret: LS, socket: Arc<dyn AsyncUdpSocket>, runtime: Arc<dyn Runtime>) -> io::Result<Self>
@@ -99,7 +97,7 @@ impl NsqServer {
             Some(NsqServerConfig::new(local_static_secret).server_config()),
             socket,
             runtime,
-        ).map(|endpoint| Self(endpoint))
+        ).map(Self)
     }
 
     pub fn endpoint(self) -> Endpoint {
@@ -108,62 +106,5 @@ impl NsqServer {
 
     pub fn endpoint_as_ref(&self) -> &Endpoint {
         &self.0
-    }
-}
-
-// TODO: Move to config.rs?
-fn nsq_resolver() -> BoxedCryptoResolver {
-    Box::new(FallbackResolver::new(Box::new(DefaultResolver), Box::new(SnowX448Resolver)))
-}
-
-const NSQ_PROTOCOL_STRING: &str = "Noise_XXhfs_448+Kyber1024_ChaChaPoly_BLAKE2b";
-
-#[cfg(test)]
-mod tests {
-    use rand::rngs::OsRng;
-    use snow::Builder;
-    use x448::Secret;
-
-    use super::*;
-
-    #[test]
-    fn nsq_round_trip_ok() {
-        let initiator_static_secret = Secret::new(&mut OsRng);
-        let responder_static_secret = Secret::new(&mut OsRng);
-
-        let mut initiator = Builder::with_resolver(NSQ_PROTOCOL_STRING.parse().expect("Should parse"), nsq_resolver())
-            .local_private_key(initiator_static_secret.as_bytes())
-            .build_initiator()
-            .expect("Should build");
-
-        let mut responder = Builder::with_resolver(NSQ_PROTOCOL_STRING.parse().expect("Should parse"), nsq_resolver())
-            .local_private_key(responder_static_secret.as_bytes())
-            .build_responder()
-            .expect("Should build");
-
-        let mut noise_message = vec![0u8; 4096];
-        let mut payload = vec![0u8; 32];
-
-        let message_len = initiator.write_message(&[], &mut noise_message).expect("Should write");
-        let payload_len = responder.read_message(&noise_message[..message_len], &mut payload).expect("Should read");
-        assert_eq!(0, payload_len);
-
-        let message_len = responder.write_message(&[], &mut noise_message).expect("Should write");
-        let payload_len = initiator.read_message(&noise_message[..message_len], &mut payload).expect("Should read");
-        assert_eq!(0, payload_len);
-
-        let message_len = initiator.write_message(&[], &mut noise_message).expect("Should write");
-        let payload_len = responder.read_message(&noise_message[..message_len], &mut payload).expect("Should read");
-        assert_eq!(0, payload_len);
-
-        assert!(initiator.is_handshake_finished(), "Initiator state should be handshake finished");
-        assert!(responder.is_handshake_finished(), "Responder state should be handshake finished");
-
-        let mut initiator = initiator.into_transport_mode().expect("Should convert");
-        let mut responder = responder.into_transport_mode().expect("Should convert");
-
-        let message_len = initiator.write_message(b"hello", &mut noise_message).expect("Should write");
-        let payload_len = responder.read_message(&noise_message[..message_len], &mut payload).expect("Should read");
-        assert_eq!(b"hello", &payload[..payload_len]);
     }
 }
--- nsq/src/quinn_noise/config.rs
+++ nsq/src/quinn_noise/config.rs
@@ -14,11 +14,21 @@ use quinn_proto::{
 };
 use rand::{rngs::OsRng, RngCore};
 use sha2::{Digest, Sha256};
-use snow::{Builder, HandshakeState};
+use snow::{
+    resolvers::{BoxedCryptoResolver, DefaultResolver, FallbackResolver},
+    Builder,
+    HandshakeState,
+};
 
-use crate::{nsq_resolver, NSQ_PROTOCOL_STRING};
+use crate::snow_x448_resolver::SnowX448Resolver;
 
 use super::{fixed_buffer::FixedBuffer, session::NoiseSession, RETRY_KEY, RETRY_NONCE};
+
+const NSQ_PROTOCOL_STRING: &str = "Noise_XXhfs_448+Kyber1024_ChaChaPoly_BLAKE2b";
+
+fn nsq_resolver() -> BoxedCryptoResolver {
+    Box::new(FallbackResolver::new(Box::new(DefaultResolver), Box::new(SnowX448Resolver)))
+}
 
 pub(crate) struct NoiseConfig<LS> {
     local_static_secret: LS,
@@ -151,5 +161,47 @@ impl HmacKey for NoiseHmacKey {
         let mut mac = <Hmac<Sha256> as Mac>::new(&self.0.into());
         mac.update(data);
         mac.verify_slice(signature).map_err(|_| CryptoError)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::rngs::OsRng;
+    use x448::Secret;
+
+    use super::*;
+
+    #[test]
+    fn round_trip_ok() {
+        let initiator_static_secret = Secret::new(&mut OsRng);
+        let responder_static_secret = Secret::new(&mut OsRng);
+
+        let mut initiator = NoiseConfig::new(initiator_static_secret.as_bytes()).new_initiator_handshake_state();
+        let mut responder = NoiseConfig::new(responder_static_secret.as_bytes()).new_responder_handshake_state();
+
+        let mut noise_message = vec![0u8; 4096];
+        let mut payload = vec![0u8; 32];
+
+        let message_len = initiator.write_message(&[], &mut noise_message).expect("Should write");
+        let payload_len = responder.read_message(&noise_message[..message_len], &mut payload).expect("Should read");
+        assert_eq!(0, payload_len);
+
+        let message_len = responder.write_message(&[], &mut noise_message).expect("Should write");
+        let payload_len = initiator.read_message(&noise_message[..message_len], &mut payload).expect("Should read");
+        assert_eq!(0, payload_len);
+
+        let message_len = initiator.write_message(&[], &mut noise_message).expect("Should write");
+        let payload_len = responder.read_message(&noise_message[..message_len], &mut payload).expect("Should read");
+        assert_eq!(0, payload_len);
+
+        assert!(initiator.is_handshake_finished(), "Initiator state should be handshake finished");
+        assert!(responder.is_handshake_finished(), "Responder state should be handshake finished");
+
+        let mut initiator = initiator.into_transport_mode().expect("Should convert");
+        let mut responder = responder.into_transport_mode().expect("Should convert");
+
+        let message_len = initiator.write_message(b"hello", &mut noise_message).expect("Should write");
+        let payload_len = responder.read_message(&noise_message[..message_len], &mut payload).expect("Should read");
+        assert_eq!(b"hello", &payload[..payload_len]);
     }
 }
